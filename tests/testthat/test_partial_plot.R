3ab946f9719e26ee5b86d34485529ba34905feb9
mattmills49 <mattmills49@gmail.com>
2016-07-15 10:57:18 -0400

    Added tests for partial_plot; check other smoothing terms work

---
+++ tests/testthat/test_partial_plot.R
@@ -0,0 +1,14 @@
+context("Partial Plot")
+library(mgcv)
+
+test_that("partial plot accepts multiple smoothing parameters", {
+  s_gam <- gam(mpg ~ s(hp), data = mtcars)
+  te_gam <- gam(mpg ~ te(hp), data = mtcars)
+  ti_gam <- gam(mpg ~ ti(hp), data = mtcars)
+  t2_gam <- gam(mpg ~ t2(hp), data = mtcars)
+  
+  expect_is(partial_plot(s_gam, "hp"), "ggplot2")
+  expect_is(partial_plot(te_gam, "hp"), "ggplot2")
+  expect_is(partial_plot(ti_gam, "hp"), "ggplot2")
+  expect_is(partial_plot(t2_gam, "hp"), "ggplot2")
+})
