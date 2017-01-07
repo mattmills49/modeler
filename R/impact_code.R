5374c0aabb73a46292ebc69f98f6641017ef6203
mattmills49 <mattmills49@gmail.com>
2017-01-07 11:30:09 -0500

    Framework for NSE with formulas and non-binary dependent variables

--- R/impact_code.R
+++ R/impact_code.R
@@ -4,44 +4,38 @@
 #' using Impact Coding described in this \href{http://helios.mm.di.uoa.gr/~rouvas/ssi/sigkdd/sigkdd.vol3.1/barreca.pdf}{paper.}
 #' 
 #' @param .data the data frame the data is contained in
-#' @param ... the variable to transform
-#' @param y the dependent variable
+#' @param formula a formula specifying the dependent and independent variables to use
 #' @param binary is the dependent variable a binary variable? Logical value
 #' @export
 #' @import magrittr
 #' 
 #' 
 
-impact_code <- function(.data, ..., y, binary = T){
+impact_code <- function(.data, formula, binary = T){
   stopifnot(is.data.frame(.data))
-  y_name <- as.character(substitute(list(y)))[-1]
-  stopifnot(all(!is.na(.data[[y_name]])))
+  y_var <- lazyeval::f_eval_lhs(formula, data = .data)
+  stopifnot(all(!is.na(y_var)))
   
-  x_vars <- dplyr::select(.data, ...)
+  x_names <- attr(terms(formula), "term.labels")
+  x_vars <- dplyr::select_(.data, .dots = x_names)
   
   if(binary){
-    codings <- smoother_binomial(x = x_vars, y = .data[[y_name]])
-  } else codings <- smoother_gaussian(x = x_vars, y = .data[[y_name]])
+    codings <- smoother_binomial(x = x_vars, y = y_var, groups = x_names)
+  } else codings <- smoother_gaussian(x = x_vars, y = y_var, groups = x_names)
   
-  return(codings)
+  
+  return(dplyr::select_(codings, .dots = c(x_names, "estimate")))
 }
 
 #' 
 
-smoother_gaussian <- function(x, y){
-  group_names <- sort(unique(x))
-  group_counts <- vapply(group_names, function(gr) sum(x == gr), numeric(1))
+smoother_gaussian <- function(x, y, groups){
+  impact_df <- dplyr::mutate(x, .y = y) %>%
+    dplyr::mutate(y_var = var(.y), y_avg = mean(.y)) %>%
+    dplyr::group_by_(.dots = c(groups, "y_var", "y_avg")) %>%
+    dplyr::summarize(.count = n(), .avg = mean(.y), .var = var(.y)) %>%
+    dplyr::mutate(.smooth = y_var / (.var / .count + y_var),
+                  estimate = .smooth * .avg + (1 - .smooth) * .avg)
   
-  group_probs <- vapply(group_names, function(gr) mean(y[x == gr]), numeric(1))
-  
-  y_var <- var(y)
-  y_prob <- mean(y)
-  
-  group_var <- vapply(group_names, function(gr) var(y[x == gr]), numeric(1))
-  smoother <- rep(0, length(group_names))
-  names(smoother) <- group_names
-  for(gr in group_names){
-    smoother[gr] <- y_var / (group_var[gr] / group_counts[gr] +  y_var)
-  }
-  posterior <- smoother * group_probs + (1 - smoother) * y_prob
+  return(impact_df)
 }
--- tests/testthat/test_impact_code.R
+++ tests/testthat/test_impact_code.R
@@ -9,3 +9,5 @@ test_that("impact_code stops if missing variables in the y variable", {
   bad_iris$Pedal.Width[20] <- NA
   expect_error(impact_code(bad_iris, Species, y = Petal.Width))
 })
+
+#' test for variable names, small groups, 1 and multiple variables
