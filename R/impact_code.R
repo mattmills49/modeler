c3276c3de32abba413b0b44163e1a4e739c5f4fa
mattmills49 <mattmills49@gmail.com>
2017-01-07 13:42:36 -0500

    added smoother_binomial helper to impact_code

--- R/impact_code.R
+++ R/impact_code.R
@@ -8,8 +8,9 @@
 #' @param binary is the dependent variable a binary variable? Logical value
 #' @export
 #' @import magrittr
-#' 
-#' 
+#' @examples 
+#' impact_code(mtcars, am ~ cyl)
+#' impact_code(iris, Petal.Width ~ Species, binary = F)
 
 impact_code <- function(.data, formula, binary = T){
   stopifnot(is.data.frame(.data))
@@ -20,8 +21,8 @@ impact_code <- function(.data, formula, binary = T){
   x_vars <- dplyr::select_(.data, .dots = x_names)
   
   if(binary){
-    codings <- smoother_binomial(x = x_vars, y = y_var, groups = x_names)
-  } else codings <- smoother_gaussian(x = x_vars, y = y_var, groups = x_names)
+    codings <- smoother_binomial(y = y_var, x = x_vars, groups = x_names)
+  } else codings <- smoother_gaussian(y = y_var, x = x_vars, groups = x_names)
   
   
   return(dplyr::select_(codings, .dots = c(x_names, "estimate")))
@@ -29,13 +30,37 @@ impact_code <- function(.data, formula, binary = T){
 
 #' 
 
-smoother_gaussian <- function(x, y, groups){
+smoother_gaussian <- function(y, x, groups){
   impact_df <- dplyr::mutate(x, .y = y) %>%
     dplyr::mutate(y_var = var(.y), y_avg = mean(.y)) %>%
     dplyr::group_by_(.dots = c(groups, "y_var", "y_avg")) %>%
     dplyr::summarize(.count = n(), .avg = mean(.y), .var = var(.y)) %>%
     dplyr::mutate(.smooth = y_var / (.var / .count + y_var),
-                  estimate = .smooth * .avg + (1 - .smooth) * .avg)
+                  estimate = .smooth * .avg + (1 - .smooth) * .avg) %>%
+    dplyr::ungroup()
+  
+  return(impact_df)
+}
+
+smoother_binomial <- function(y, x, groups) UseMethod("smoother_binomial")
+
+smoother_binomial.factor <- function(y, x, groups) smoother_binomial.numeric(1 * (y == levels(y)[1]), x, groups)
+
+smoother_binomial.logical <- function(y, x, groups) smoother_binomial.numeric(1 * y, x, groups)
+
+smoother_binomial.character <- function(y, x, groups) smoother_binomial.numeric(1 * (y == y[1]), x, groups)
+
+smoother_binomial.numeric <- function(y, x, groups){
+  impact_df <- dplyr::mutate(x, .y = y) %>% 
+    dplyr::group_by_(.dots = c(groups)) %>%
+    dplyr::summarize(.count = n(), .yes = sum(.y)) %>% 
+    dplyr::mutate(.probs = .yes / .count) %>%
+    dplyr::ungroup() %>%
+    dplyr::mutate(.u = mean(.probs), 
+                  .v = var(.probs),
+                  .a = ((1 - .u) / .v - 1 / .u) * .u * .u,
+                  .b = .a * (1 / .u - 1),
+                  estimate = (.yes + .a) / (.a + .b + .count))
   
   return(impact_df)
 }
--- tests/testthat/test_impact_code.R
+++ tests/testthat/test_impact_code.R
@@ -1,13 +1,30 @@
 context("impact_code")
 
-test_that("impact_code only accepts data frames", {
-  expect_error(impact_code(iris$Species, Species, y = test))
+test_that("impact_code erros", {
+  bad_iris <- iris
+  bad_iris$Pedal.Width[20] <- NA
+  
+  expect_error(impact_code(bad_iris, Petal.Width ~ Species, binary = F), info = "Y can't have missing values")
+  expect_error(impact_code(iris$Species, Petal.Width ~ Species, binary = F), info = "only works on data frames")
 })
 
 test_that("impact_code stops if missing variables in the y variable", {
-  bad_iris <- iris
-  bad_iris$Pedal.Width[20] <- NA
-  expect_error(impact_code(bad_iris, Species, y = Petal.Width))
+  
 })
 
-#' test for variable names, small groups, 1 and multiple variables
+#' test for variable names, 
+#' small groups, 
+#' 1 and multiple variables, 
+#' missing values
+#' more than 3 values
+
+test_that("impact_code works on continous variables", {
+  expect_is(impact_code(iris, Petal.Width ~ Species, binary = F), "data.frame")
+})
+
+test_that("impact_code works on different classes of variables", {
+  expect_is(impact_code(mtcars, am ~ cyl), "data.frame")
+  expect_is(impact_code(mtcars, factor(am) ~ cyl), "data.frame")
+  expect_is(impact_code(mtcars, am == 1 ~ cyl), "data.frame")
+  expect_is(impact_code(mtcars, as.character(am) ~ cyl), "data.frame")
+})
