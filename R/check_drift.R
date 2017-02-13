3847ff475200fb87ede49f06473630ae07f2acf5
mattmills49 <mattmills49@gmail.com>
2017-02-13 10:16:05 -0500

    documented check_drift

--- R/check_drift.R
+++ R/check_drift.R
@@ -10,8 +10,11 @@
 #' @param bins the number of bins to make. If \code{NULL} then each variable 
 #' value will be used. 
 #' 
+#' @return a ggplot2 object showing the distribution of the variable colored by
+#' group
+#' 
 #' @export 
-#' @example 
+#' @examples
 #' check_drift(iris, Petal.Width ~ Species, bins = 5)
 #' check_drift(mtcars, cyl ~ am, bins = NULL)
 
@@ -29,7 +32,10 @@ check_drift <- function(.data, formula, bins = 10){
   
 }
 
+
 drift_check <- function(x_values, x_name, drift_values, drift_name, .data, bins) UseMethod("drift_check")
+
+drift_check.default <- function(x_values, x_name, drift_values, drift_name, .data, bins) drift_check.numeric(x_values, x_name, drift_values, drift_name, .data, bins)
 
 drift_check.numeric <- function(x_values, x_name, drift_values, drift_name, .data, bins){
   
@@ -59,7 +65,7 @@ drift_check.numeric <- function(x_values, x_name, drift_values, drift_name, .dat
   
 }
 
-drift_check.numeric <- function(x_values, x_name, drift_values, drift_name, .data, bins){
+drift_check.character <- function(x_values, x_name, drift_values, drift_name, .data, bins){
   
   if(is.null(bins)){
     
@@ -67,10 +73,13 @@ drift_check.numeric <- function(x_values, x_name, drift_values, drift_name, .dat
     
   } else {
     
-    break_locs <- unique(quantile(x_values, probs = seq(0, 1, length.out = bins + 1)))
+    break_values <- unique(x_values)
+    names(break_values) <- break_values
+    top_n <- names(sort(table(x_values), decreasing = T))[seq_len(bins)]
+    break_values[!(break_values %in% top_n)] <- "other"
     
     plot_data <- dplyr::data_frame(x = x_values, drift = as.character(drift_values)) %>%
-      dplyr::mutate(x_groups = cut(x, breaks = break_locs, include.lowest = T))
+      dplyr::mutate(x_groups = break_values[x])
     
   }
   
@@ -86,6 +95,8 @@ drift_check.numeric <- function(x_values, x_name, drift_values, drift_name, .dat
     ggplot2::ggtitle(stringr::str_c("Change in ", x_name, " by ", drift_name))
   
 }
+
+drift_check.factor <- function(x_values, x_name, drift_values, drift_name, .data, bins) drift_check.character(as.character(x_values), x_name, drift_values, drift_name, .data, bins)
 
 # .data %>%
 #   dplyr::mutate_(.dots = setNames(list(~ as.character(drift_name)), drift_name)) %>%
